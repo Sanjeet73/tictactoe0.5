aa543f510ef8666a666108099e5b592ba0387b7c
Sanjeet Git <73155546+Sanjeet73@users.noreply.github.com>
2022-12-25 16:02:50 +0530

    ho gaya board and squares

---
+++ src/components/Squares.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Squares = ({value}) => {
+  return (
+    <button type ="button">{value}</button>
+  )
+}
+
+export default Squares
---
+++ src/components/board.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Squares from './Squares'
+const Board = ({sanjeet}) => {
+  return (
+    <div>
+    <div>
+    
+      <Squares value={0}/>
+      <Squares value={1}/>
+      <Squares value={2}/>
+      
+    </div>
+     <div>
+    
+     <Squares value={3}/>
+     <Squares value={4}/>
+     <Squares value={5}/>
+     
+   </div>
+    <div>
+    
+    <Squares value={6}/>
+    <Squares value={7}/>
+    <Squares value={8}/>
+    
+  </div>
+  </div>
+  )
+}
+
+export default Board
